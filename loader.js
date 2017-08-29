b12e58d430738722f6e20633b1eff2bad831769c
fie2377 <fie2377@gmail.com>
2017-08-29 19:26:57 -0300

    apagamos o conteudo do loade.js e carregamos com reqire

--- loader.js
+++ loader.js
@@ -1,17 +1,3 @@
-const port = 3003
 
-const bodyParser = require("body-parser")
-
-const express = require("express")
-const server = express()
-
-server.use(bodyParser.urlencoded({extended: true}))
-server.use(bodyParser.json())
-
-server.listen(port, function(){
-  console.log(`backend is runing in port ${port}`)
-})
-
-server.use(function(req, res, next){
-  res.send("Funcionou!")
-})
+require("./config/server")
+require('./config/database')
