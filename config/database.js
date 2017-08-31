f7900b8a8a10c78461f946677aa35ec7b7cc113e
fie2377 <fie2377@gmail.com>
2017-08-31 17:19:50 -0300

    configuração das mensagens de erros no arquivo database.js

--- config/database.js
+++ config/database.js
@@ -1,3 +1,6 @@
 const mongoose = require("mongoose")
 
 module.exports = mongoose.connect('mongodb://localhost/db_finance')
+
+mongoose.Error.messages.general.required = "O atributo '{PATH} é obrigatótio.'"
+mongosse.Error.messages.Number.min = "O {VALUE} informado é menor que o limite minimo de '{MIN}'."
