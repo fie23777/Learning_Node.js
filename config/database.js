b018d26fb9881a0994af86bdc94c8d765496f8ac
fie2377 <fie2377@gmail.com>
2017-08-31 17:24:47 -0300

    Mensagens de erros para valores máximo e enum

--- config/database.js
+++ config/database.js
@@ -3,4 +3,6 @@ const mongoose = require("mongoose")
 module.exports = mongoose.connect('mongodb://localhost/db_finance')
 
 mongoose.Error.messages.general.required = "O atributo '{PATH} é obrigatótio.'"
-mongosse.Error.messages.Number.min = "O {VALUE} informado é menor que o limite minimo de '{MIN}'."
+mongosse.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite minimo de '{MIN}'."
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'."
+mongosse.Error.messages.String.enum = "'{VALUE}' não é válido para o atributo '{PATH}'"
